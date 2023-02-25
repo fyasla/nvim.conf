a26aa8c516a36e32179884c84abf1f268d2c8a7c
fyasla <f.bougamale@gmail.com>
2023-02-25 03:31:36 +0100

    add Vs code theme, transparent background

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  { "Mofiqul/vscode.nvim", opts = {
+    transparent = true,
+  } },
+
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+}
